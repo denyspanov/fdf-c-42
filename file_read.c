272eea3247d5bd9b0c6da9b7f0d0532ccab1009b
denyspanov <denys.panov@gmail.com>
2017-03-14 14:28:30 +0200

    sa

--- file_read.c
+++ file_read.c
@@ -11,104 +11,63 @@
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include <math.h>
 
-void line_draw(float x1, float x2, float y1, float y2, void *mlx, void *wnd)
+t_mas     *mas_size(int fd, t_mas **data)
 {
-	double dx, dy, p, end;
-	float x, y;
-	dx = fabs(x1 - x2);
-	dy = fabs(y1 - y2);
-	p = 2 * dy - dx;
-	if(x1 > x2)
-	{
-		x = x2;
-		y = y2;
-		end = x1;
-	}
-	else
-	{
-		x = x1;
-		y = y1;
-		end = x2;
-	}
-	mlx_pixel_put(mlx, wnd, x, y, 0x00FFFFFF);
-	while(x < end)
-	{
-		x = x + 1;
-		if(p < 0)
-		{
-			p = p + 2 * dy;
-		}
-		else
-		{
-			y = y + 1;
-			p = p + 2 * (dy - dx);
-		}
-		mlx_pixel_put(mlx, wnd, x, y, 0x00FFFFFF);
-	}
-}
-
-void draw(t_mas **mas)
-{
-	int y;
-	int x;
-	void *mlx;
-	void *wnd;
-	int counter;
-
-	counter = 20;
-	mlx = mlx_init();
-	wnd = mlx_new_window(mlx, 1000, 1000, "FDF");
-	y = -1;
-	while (++y < (*mas)->y)
-	{
-		x = -1;
-		while (++x < (*mas)->x)
-		{
-			line_draw((*mas)->mas[y][x] + x *counter + 100,(*mas)->mas[y][x] + x * counter + 200, (*mas)->mas[y][x] + y * counter + 100,(*mas)->mas[y][x] + y * counter + 100, mlx, wnd);
-			counter = 20;
-		}
-		line_draw((*mas)->mas[y][x] + x *counter + 100,(*mas)->mas[y][x] + x * counter + 100, (*mas)->mas[y][x] + y * counter + 200,(*mas)->mas[y][x] + y * counter + 200, mlx, wnd);
-	}
-	mlx_loop(mlx);
-}
-
-int mas_create(int fd)
-{
-	int i;
-	int j;
-	int x;
 	char *line;
-	char *temp;
-	t_mas *mas;
+	int i;
 
-	mas = (t_mas *)malloc(sizeof(t_mas));
-	mas->mas = (int **)malloc(sizeof(int **));
-	mas->y= 0;
-	mas->x = 0;
-	while ((get_next_line(fd,&line))> 0)
+	while ((get_next_line(fd, &line)) > 0)
 	{
-		mas->mas[mas->y] = (int *)malloc(sizeof(int *));
-		i = -1;
-		x = 0;
-		while (line[++i])
+		i = 0;
+		(*data)->x = -1;
+		while (line[i++] != '\0')
 		{
 			if (line[i] > 47 && line[i] < 58)
 			{
-				j = i;
 				while (line[i] > 47 && line[i] < 58)
 					i++;
-				temp = ft_strsub(line, j, i - j);
-				mas->mas[mas->y][x] = ft_atoi(temp);
-				x++;
-				mas->x++;
+				(*data)->x++;
 			}
 		}
-		mas->y++;
+		(*data)->y++;
 	}
-	mas->x /= mas->y;
-	draw(&mas);
-	return (0);
+	return (*data);
+}
+
+t_mas *mas_create(int fd, char *src)
+{
+	int i;
+	int j;
+	char *line;
+	char *temp;
+	t_mas *data;
+
+	data = (t_mas *)malloc(sizeof(t_mas));
+	data = mas_size(fd, &data);
+	close(fd);
+	fd = open(src,O_RDONLY);
+	data->mas = (int **)malloc(data->y * sizeof(int *));    
+	data->y = 0;
+	while ((get_next_line(fd, &line)) > 0)
+	{
+		data->mas[data->y] = (int *)malloc(data->x * sizeof(int *));
+		i = -1;
+		data->x = 0;
+		while (line[++i])
+		{
+			if ((line[i] > 47 && line[i] < 58) || line[i] == '-')
+			{
+				j = i;
+				while ((line[i] > 47 && line[i] < 58) || line[i] == '-')
+					i++;
+				temp = ft_strsub(line, j, i - j);
+				data->mas[data->y][data->x] = ft_atoi(temp);
+				data->x++;
+			}
+		}
+		data->y++;
+	}
+	return (data);
 }
 
--- main.c
+++ main.c
@@ -12,13 +12,92 @@
 
 #include "fdf.h"
 
+void line_draw(float x1, float x2, float y1, float y2, void *mlx, void *wnd)
+{
+	double dx, dy, p, end;
+	float x, y;
+	dx = fabs(x1 - x2);
+	dy = fabs(y1 - y2);
+	p = 2 * dy - dx;
+	if(x1 > x2)
+	{
+		x = x2;
+		y = y2;
+		end = x1;
+	}
+	else
+	{
+		x = x1;
+		y = y1;
+		end = x2;
+	}
+	mlx_pixel_put(mlx, wnd, x, y, 0x00FFFFFF);
+	while(x < end)
+	{
+		x = x + 1;
+		if(p < 0)
+		{
+			p = p + 2 * dy;
+		}
+		else
+		{
+			y = y + 1;
+			p = p + 2 * (dy - dx);
+		}
+		mlx_pixel_put(mlx, wnd, x, y, 0x00FFFFFF);
+	}
+}
+
+void draw(t_mas **data)
+{
+	int y;
+	int x;
+	void *mlx;
+	void *wnd;
+	int counter;
+
+	counter = 10;
+	mlx = mlx_init();
+	wnd = mlx_new_window(mlx, 2000, 2000, "FDF");
+	y = -1;
+	while (++y < (*data)->y)
+	{
+		x = -1;
+		counter = 10;
+		while (++x < (*data)->x)
+		{
+			if (x + 1 != (*data)->x)
+				line_draw((*data)->mas[y][x] + counter, (*data)->mas[y][x] + counter,(*data)->mas[y][x] + y * 10,(*data)->mas[y][x] + y * 10,mlx, wnd);
+			counter += 10;
+		}
+	}
+	mlx_loop(mlx);
+}
+
 int main(int argc, char **argv)
 {
 	int fd;
+	t_mas *data;
+	int i;
+	int j;
 
-	if (argc == 2)
+	if (argc > 1)
 	{
 		fd = open(argv[1],O_RDONLY);
-		mas_create(fd);
+		if (fd < 0)
+			return (0);
+		data = mas_create(fd, argv[1]);
+		draw(&data);
+		/*j = -1;
+		while (++j < data->y)
+		{
+			i = -1;
+			while (++i < data->x)
+			{
+				printf("%d  ",data->mas[j][i]);
+			}
+			printf("\n");
+		}*/
 	}
+	return (0);
 }
